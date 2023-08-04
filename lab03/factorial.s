c93c021b41a97c283cbbe08fbace53f05434e6ff
Muhammad Hayat <muhammad.hayat@10xengineers.ai>
2023-08-04 12:00:24 +0500

    Task of Lab03 Exercise 4

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,24 +1,24 @@
-.globl factorial
-
 .data
 n: .word 8
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
-    jal ra, factorial
+    la s2, n         # load the address of n in s2
+    addi t0, x0, 1   # Initialize loop counter t0 to 1
+    addi t1, x0, 1   # Initialize factorial result t1 to 1
+    lw t3, 0(s2)     # load the value at address s2 in t3
 
-    addi a1, a0, 0
-    addi a0, x0, 1
-    ecall # Print Result
-
+loop:
+    mul t1, t1, t0   # Multiply the factorial result t1 by the loop counter t0
+    addi t0, t0, 1   # Increment the loop counter t0
+    ble t0, t3, loop  # Check if t0 is less than or equal to n, if true, continue the loop
+    add a1, x0, t1
+    li a0, 1
+    ecall
     addi a1, x0, '\n'
-    addi a0, x0, 11
-    ecall # Print newline
+    addi a0,x0,11
+    ecall
+    li a0, 10
+    ecall
 
-    addi a0, x0, 10
-    ecall # Exit
-
-factorial:
-    # YOUR CODE HERE
+    
